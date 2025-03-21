c1931479a637614ed02a3a507b9f71e3c5cb4684
adrie <zeyroxjeu@gmail.com>
2025-03-21 12:02:37 +0100

    yes

--- main.c
+++ main.c
@@ -2,9 +2,5 @@
 
 int main(void) {
     printf("Hello, World!\n");
-    printf("Working now ?");
-    printf("Couscous");
-    printf("Tajine");
-    printf("Si ça marche as cette fois jme pends");
     return 0;
 }
