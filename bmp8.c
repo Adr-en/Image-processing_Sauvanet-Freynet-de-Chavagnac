3f30abcfec7bec7b209db5184ce7cfaae9fb6d2e
Ethan-Sauvanet <ethan.sauvanet@efrei.net>
2025-04-26 18:56:06 +0200

    bmp8.h.c into bmp8.h

--- bmp8.c
+++ bmp8.c
@@ -1,6 +1,6 @@
 # include <stdio.h>
 # include <stdlib.h>
-#include "bmp8.h.c"
+#include "bmp8.h"
 
 t_bmp8 * bmp8_loadImage(const char * filename){
   t_bmp8 * p_image = (t_bmp8 *) malloc(sizeof(t_bmp8));
