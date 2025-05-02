3b52b2d7a0fb630a3e0e133c22b287a9b13d376e
adrie <zeyroxjeu@gmail.com>
2025-05-02 11:46:13 +0200

    update du main

--- main.c
+++ main.c
@@ -5,11 +5,12 @@
 
 int main() {
     // Load the image
-    t_bmp8* img = bmp8_loadImage("C:\\Users\\adrie\\CLionProjects\\Image-Processing_\\lena_gray.bmp");
+    t_bmp8* img = bmp8_loadImage("lena_gray.bmp");
     if (img == NULL) {
         printf("Failed to load image.\n");
         return 1;
     }
+    t_bmp8* img_temp = img;
 
     // Print image information
     bmp8_printInfo(img);
@@ -21,6 +22,15 @@ int main() {
     // Save the modified image
     bmp8_saveImage("lena_gray_negative.bmp", img);
     printf("Image saved as barbara_gray_negative.bmp\n");
+
+    img = img_temp;
+    bmp8_brightness(img, 50);
+    bmp8_saveImage("lena_gray_brightness.bmp", img);
+
+    //img = img_temp;
+    //bmp8_threshold(img, 128);
+    //bmp8_saveImage("lena_gray_threshold.bmp", img);
+
 
     // Free the memory
     bmp8_free(img);
